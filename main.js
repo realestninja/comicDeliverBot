7f34703651c6303feeaac857ffa70c2990e3b840
realestninja <nikhil@realest.ninja>
2016-09-16 19:09:10 +0200

    added features: pick latest comic, pick a comic by number

--- main.js
+++ main.js
@@ -10,18 +10,39 @@ bot.on('message', function(json) {
   if (json.hasOwnProperty('text')) {
     var chatID = json.chat.id;
     var message = json.text;
+    var splitMessage = message.split(/[ ,]+/);
 
-    if (message == '!comic') {
-      request('http://explosm.net/comics/random', function(error, response, body) {
-        if(!error && response.statusCode == 200) {
-          $ = cheerio.load(body);
-          image = $('#main-comic').attr('src').slice(2);
-          sendComic(chatID, image);
-        }
-      });
+    var comicType = splitMessage[0];
+    var comicNumber = splitMessage[1];
+
+    switch(comicType) {
+      case '!explosm':
+        getExplosm(comicNumber, chatID);
+        break;
+      default:
+        break;
     }
   }
 });
+
+function getExplosm(number, chatID) {
+  var url = 'http://explosm.net/';
+  var container = '#featured-comic';
+
+  if (number != 'latest') {
+    url += 'comics/';
+    url += number != null ? number : 'random';
+    container = '#main-comic';
+  }
+
+  request(url, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      $ = cheerio.load(body);
+      image = $(container).attr('src').slice(2);
+      sendComic(chatID, image);
+    }
+  });
+}
 
 function sendComic(chatID, link) {
   bot.sendMessage(chatID, link);
