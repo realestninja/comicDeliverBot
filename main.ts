9d0837285516ac43df2c0f039d226cce9195d6f0
nikhil <the@realest.ninja>
2017-09-10 15:28:40 +0200

    Refactor main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,31 @@
-var token = '';
+import TelegramBot from 'node-telegram-bot-api';
+import cheerio from 'cheerio';
+import requestPromise from 'request-promise';
 
-var TelegramBot = require('node-telegram-bot-api');
-var bot = new TelegramBot(token, {polling: true});
-var cheerio = require('cheerio');
-var rp = require('request-promise');
+const token = '';
+const bot = new TelegramBot(token, { polling: true });
 
 console.log('bot running');
 
-bot.on('message', function(json) {
+bot.on('message', json => {
   if (json.hasOwnProperty('text')) {
-    var chatID = json.chat.id; 
-    var message = json.text;
-    var splitMessage = message.split(/[ ,]+/);
-    var comicType = splitMessage[0]; //first word is the comic type. for now either /xkcd or /explosm
-    var comicNumber = splitMessage[1]; //can be left empty. otherwise either a number or 'latest'
+    const chatID = json.chat.id;
+    const message = json.text;
+    const splitMessage = message.split(/[ ,]+/);
+    const comicType = splitMessage[0]; // first word is the comic type. for now either /xkcd or /explosm
+    const comicNumber = splitMessage[1]; // can be left empty. otherwise either a number or 'latest'
 
-    switch(comicType) {
+    switch (comicType) {
       case '/explosm':
         getExplosm(comicNumber, chatID);
-      break;
+        break;
 
       case '/xkcd':
         getXKCD(comicNumber, chatID);
-      break;
+        break;
 
-      default: break;
+      default:
+        break;
     }
   }
 });
@@ -32,7 +33,7 @@ bot.on('message', function(json) {
 function sendComic(chatID, img, text) {
   bot.sendMessage(chatID, img);
 
-  if(text) {
+  if (text) {
     bot.sendMessage(chatID, text);
   }
 
@@ -40,27 +41,28 @@ function sendComic(chatID, img, text) {
 }
 
 function getExplosm(number, chatID) {
-  var url = 'http://explosm.net/';
-  var container = number == 'latest' ? '#featured-comic' : '#main-comic';
-  var image;
+  let url = 'http://explosm.net/';
+  const container = number === 'latest' ? '#featured-comic' : '#main-comic';
+  let image;
   /*
   -'main-comic' is the container ID for comics
   -however, the latest comic has a different cotainer ID which is named 'featured-comic'
   */
 
-  if (number != 'latest') {
+  if (number !== 'latest') {
     url += 'comics/';
     url += number != null ? number : 'random';
   }
 
-  rp(url).then(function(data) {
-    var $ = cheerio.load(data); //store DOM-like content
-    image = $(container).attr('src').slice(2); //use cheerio (jquery like) to get 'src' attr
-    sendComic(chatID, image);
+  requestPromise(url).then(data => {
+    const $ = cheerio.load(data); // store DOM-like content
+    const positionToCut = 2;
+    image = $(container).attr('src').slice(positionToCut); // use cheerio (jquery like) to get 'src' attr
+    sendComic(chatID, image, null);
   })
-  .catch(function(error) {
-    console.log(error);
-  });
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function getXKCD(number, chatID) {
@@ -71,38 +73,37 @@ function getXKCD(number, chatID) {
   -url is initially set to the latest comic.
   -JSON of the latest comic also has the information about total amount of available comics
   */
-  var url = 'http://xkcd.com/info.0.json'; 
-  var amountComics; //required to create a random number within the range of existing comics
+  let url = 'http://xkcd.com/info.0.json';
+  let amountComics; // required to create a random number within the range of existing comics
 
-  //if number == latest -> send the last comic that was released
-  if (number == 'latest') {
+  // if number == latest -> send the last comic that was released
+  if (number === 'latest') {
     handleXkcdRequest(chatID, url);
   } else {
-    //if a specific or random comic was requested -> get the amount of existing comics first
-    rp({url: url, json: true}).then(function(data) {
+    // if a specific or random comic was requested -> get the amount of existing comics first
+    requestPromise({ json: true, url }).then(data => {
       amountComics = data.num;
     })
-    .then(function() {
-      if (number == null || number > amountComics) {
-        //set number to a random value if no number is given or if given number exceeds the range
-        number = Math.floor(Math.random() * amountComics) + 1;
-      }
+      .then(() => {
+        if (number == null || number > amountComics) {
+          // set number to a random value if no number is given or if given number exceeds the range
+          number = Math.floor(Math.random() * amountComics) + 1;
+        }
 
-      //change the API url to the corresponding request
-      url = 'http://xkcd.com/' + number + '/info.0.json';
-      handleXkcdRequest(chatID, url);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+        // change the API url to the corresponding request
+        url = 'http://xkcd.com/' + number + '/info.0.json';
+        handleXkcdRequest(chatID, url);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 }
 
 function handleXkcdRequest(chatID, url) {
-  rp({url: url, json: true})
-    .then(function(data) {
-      sendComic(chatID, data.img, 'No.' + data.num + ": " + data.alt);
-      //data.alt is an alternative text which is featured on every xkcd comic
-    }
-  ); 
+  requestPromise({ json: true, url })
+    .then(data => {
+      sendComic(chatID, data.img, 'No.' + data.num + ': ' + data.alt);
+      // data.alt is an alternative text which is featured on every xkcd comic
+    });
 }
