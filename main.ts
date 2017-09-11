ce8a3e21bfe1e9d826f53a1f601b2fdde48d6fe0
nikhil <the@realest.ninja>
2017-09-11 21:44:06 +0200

    Fix a bug and cleanup

--- main.ts
+++ main.ts
@@ -3,17 +3,14 @@ import * as TelegramBot from 'node-telegram-bot-api';
 import { getExplosm } from './src/comic_types/getExplosm';
 import { getXKCD } from './src/comic_types/getXKCD';
 
-import { token } from './settings';
-import { botSettings } from './settings';
-import { url } from './settings';
+import { token, botSettings, url } from './settings';
 
+/*
 const bot = new TelegramBot(token, botSettings);
 bot.setWebHook(url);
+*/
 
-// Just to ping!
-bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.id, 'I am alive on uberspace!');
-});
+const bot = new TelegramBot(token, { polling: true });
 
 console.log('bot running');
 
@@ -30,7 +27,9 @@ bot.on('message', json => {
       '/xkcd': getXKCD,
     };
 
-    const processComic = availableComicTypes[comicType];
-    processComic(bot, comicNumber, chatID);
+    if (comicType in availableComicTypes) {
+      const processComic = availableComicTypes[comicType];
+      processComic(bot, comicNumber, chatID);
+    }
   }
 });
